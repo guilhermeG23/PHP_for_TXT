9b9adc13cedbdd6d8d2c9b77220083e88f3c8c8a
guilhermeG23 <guilhermebrechot@gmail.com.br>
2019-05-07 10:13:52 -0300

    Tonando a entrada automatica a partir de determinada quantidade de caracteres

--- index.php
+++ index.php
@@ -17,30 +17,58 @@ if (isset($_POST['entrada']) && strlen($_POST['entrada']) >= 1) {
 	#Fechar o arquivo
 	fclose($abrir);
 }
+#Valor da regra do tamanho do input e do tamanho da decisao
+$tamanho_entrada = 10;
 ?>
 <style>
 /*CSS na pagina*/
+div, h1, input {
+	text-align: center;
+}
 div {
 	margin: auto;
 	width: 100%;
 	height: 100%;
-	text-align: center;
 }
 form {
 	padding: 10px;
+	border: 1px solid black;
 }
-button, input {
+h1 {
+	font-size: 32px;
+}
+input {
 	padding: 10px;
 	width: 100%;
 	margin-top: 20px;
-	text-align: center;
 	font-size: 18px;
 }
 </style>
+<script>
+//Contador de caracteres para ativar o registro automatico
+//Toda a vez que o input receber alguma entrada a funcao roda
+function calcular(entrada_input) {
+	//Pega o tamanho da valor do input entrada
+	const valor = document.getElementById('entrada');
+	//Somador por caracter
+	const contadorPorLetra = 1;
+	//Contador de espacos
+	const espacos = (entrada_input.value.split(" ").length - 1);
+	//Contar o tamanho do input
+	const caracteres = entrada_input.value.length;
+	//Fazendo a conta
+	const contador = (parseInt((caracteres - espacos) * contadorPorLetra)); 
+	//Verificando a situacao para ativar ou nao a decisao
+	if (contador >= <?=$tamanho_entrada;?>) {
+		//Caso tenha o tamanho correto, ele manda dar um submit no for, isso recarrega a pagina, ativando a decisao do php
+		document.getElementById("cadastrar").submit();
+	}
+}
+</script>
 <!--Entrada do input-->
 <div>
-	<form action="index.php" method="POST">
-		<input id="entrada" name="entrada" autocomplete="off" autofocus required></input>
-		<button type="submit">Registrar</button>
+	<form action="index.php" method="POST" id="cadastrar">
+		<h1>Entrar com o codigo de barras</h1>
+		<input id="entrada" name="entrada" onkeyup="calcular(this)" autocomplete="off" autofocus maxlength="<?=$tamanho_entrada;?>" placeholder="entrada do codigo de barras..." required></input>
 	</form>
 </div>
